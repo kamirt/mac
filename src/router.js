dbe1aef0275b25618e877e11e5456a51bb169a03
kamirt <kamirt@yandex.ru>
2019-02-18 11:35:23 +0300

    en

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.NODE_ENV === 'development' ? process.env.BASE_URL : 'mac/dist/',
   routes: [
     {
